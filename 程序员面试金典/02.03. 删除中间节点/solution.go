1ba1857ab5854eabfd8ffa448f847818be5caf74
feifeifeimoon <wangyufeimoon@gmail.com>
2021-12-20 22:57:52 +0800

    :sparkles:feat(程序员面试金典):02.03. 删除中间节点

---
+++ 程序员面试金典/02.03. 删除中间节点/solution.go
@@ -0,0 +1,31 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteNode(node *ListNode) {
+	if node == nil || node.Next == nil {
+		return
+	}
+	node.Val = node.Next.Val
+	node.Next = node.Next.Next
+}
+
+func main() {
+	l := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val: 3,
+				Next: &ListNode{
+					Val:  4,
+					Next: &ListNode{Val: 5},
+				},
+			},
+		},
+	}
+	deleteNode(l)
+}
